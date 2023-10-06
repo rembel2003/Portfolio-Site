3632c962117cee938faefe3bc6e3e1266f3b7437
Iurii Burlacov <yura.kkrut.2003@gmail.com>
2023-10-06 12:52:51 +0300

    my site

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded',() =>{
+    const menu=document.querySelector('.nav-menu'),
+        menuItem=document.querySelectorAll('nav-menu__item'),
+        hamburger=document.querySelector('.hamburger');
+    hamburger.addEventListener('click',()=>{
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+    })
+    menuItem.forEach(item=>{
+        item.addEventListener('click',()=>{
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+            item.classList.add('orange')
+        })
+    })
+})
